980005dbc94c29fd25684ca3d74e4de1bcf4a9bd
arashr88 <rezaee.arash88@gmail.com>
2023-07-27 13:07:19 -0400

    run for different seeds

--- tests/test_rmsa.py
+++ tests/test_rmsa.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import pickle
-
+import json
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,37 +11,79 @@ from optical_rl_gym.envs.rmsa_env import (
     least_loaded_path_first_fit,
     shortest_available_path_first_fit,
     shortest_path_first_fit,
+    least_congested_path_first_fit,
 )
 from optical_rl_gym.utils import evaluate_heuristic, random_policy
 
-load = 50
-logging.getLogger("rmsaenv").setLevel(logging.INFO)
+output = {}
+with open("data.json", "w") as file:
+    # Load the existing JSON data
+    json.dump(output, file)
 
-seed = 20
-episodes = 10
-episode_length = 100
+for load in range(50, 801, 50):
+#load = 300
 
-monitor_files = []
-policies = []
+    logging.getLogger("rmsaenv").setLevel(logging.INFO)
 
-# topology_name = 'gbn'
-# topology_name = 'nobel-us'
-# topology_name = 'germany50'
-with open(
-    os.path.join( "examples", "topologies", "nsfnet_chen_5-paths_6-modulations.h5"), "rb"
-) as f:
-    topology = pickle.load(f)
+    seed = 50
+    episodes = 1
+    episode_length = 1000
 
-env_args = dict(
-    topology=topology,
-    seed=10,
-    allow_rejection=True,
-    load=load,
-    mean_service_holding_time=25,
-    episode_length=episode_length,
-    num_spectrum_resources=64,
-    bit_rate_selection="discrete",
+    monitor_files = []
+    policies = []
+
+    # topology_name = 'gbn'
+    # topology_name = 'nobel-us'
+    # topology_name = 'germany50'
+    # with open(
+    #     os.path.join( "examples", "topologies", "nsfnet_chen_5-paths_6-modulations.h5"), "rb"
+    # ) as f:
+    #     topology = pickle.load(f)
+    output.update({load:{}})
+    for seeds_cnt in range(seed):
+        with open(
+            os.path.join( "examples", "topologies", "europe_network_10-paths_1-modulations.h5"), "rb"
+        ) as f:
+            topology = pickle.load(f)
+
+        env_args = dict(
+            topology=topology,
+            seed=seeds_cnt,
+            allow_rejection=True,
+            load=load,
+            mean_service_holding_time=3600,
+            episode_length=episode_length,
+            num_spectrum_resources=256,
+            bit_rate_selection="discrete",
+        )
+
+        print("STR".ljust(5), "REW".rjust(7), "STD".rjust(7))
+        seeds = [seeds_cnt]#list(range(0,episodes,1))
+        init_env = gym.make("RMSA-v0", **env_args)
+        env_rnd = SimpleMatrixObservation(init_env)
+        mean_reward_rnd, std_reward_rnd, blocking, BW_blocking = evaluate_heuristic(
+            env_rnd, least_congested_path_first_fit, n_eval_episodes=episodes, seeds = seeds
+        )
+        output[load].update({seeds_cnt:{
+                        'request_blocking':blocking,
+                        'BW_Blocking': BW_blocking
+        }})
+        with open("data.json", "w") as file:
+        # Write the updated data back to the file
+            json.dump(output, file, indent=4)
+    
+print("Rnd:".ljust(8), f"{mean_reward_rnd:.4f}  {std_reward_rnd:>7.4f}")
+print(
+    "\tBit rate blocking:",
+    (init_env.episode_bit_rate_requested - init_env.episode_bit_rate_provisioned)
+    / init_env.episode_bit_rate_requested,
 )
+print(
+    "\tRequest blocking:",
+    (init_env.episode_services_processed - init_env.episode_services_accepted)
+    / init_env.episode_services_processed,
+)
+print(init_env.topology.graph["throughput"])
 
 print("STR".ljust(5), "REW".rjust(7), "STD".rjust(7))
 
